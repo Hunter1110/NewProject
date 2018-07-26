a8c2cc47e790bf3d9088f8bf0dd4efd83fb43254
Hunter1110 <wenfeng_zhang@aopen.com>
2018-07-26 11:02:44 +0800

    modify sw path.

--- scripts/app.js
+++ scripts/app.js
@@ -361,7 +361,7 @@
 
   // add service worker code here
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(function() { console.log('Service Worker Registered'); });
   }
 })();
